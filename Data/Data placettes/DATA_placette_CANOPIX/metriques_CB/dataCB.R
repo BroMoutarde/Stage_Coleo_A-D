99cb599bd90b14152cdda514ab2bac7685513d69
Adrien Debout <adrien.debout@inrae.fr>
2025-06-13 11:44:53 +0200

    13-06-25 : avancée et clarification de la suite des nmds

--- Data/Data placettes/DATA_placette_CANOPIX/metriques_CB/dataCB.R
+++ Data/Data placettes/DATA_placette_CANOPIX/metriques_CB/dataCB.R
@@ -130,7 +130,7 @@ write.table(piege.ano$class.vec,file="",sep=";")
 # permanova
 #npmanova contraint par cldia
 adonis(speres[,5:30]~speres$ess,speres, permutations = 999, method = "bray",strata = speres$cldia)
-?adonis
+?adonis2
 
 #ANOSIM
 dist.saproclim<-vegdist(spefeu[,5:36],distance = "bray")
